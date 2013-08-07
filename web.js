e1896a4259ea86ba9d8429ad00f0a68974b4f2d5
Eric Bernardo <erikbernardo@gmail.com>
2013-08-07 00:44:54 +0000

    ez analytics and sharing

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function(request, response) {
     response.send(fileContent.toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
